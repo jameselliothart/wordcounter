6f7d9f41ac158b4621a242d54fced3b10331bb7b
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-10-10 16:48:58 -0500

    add postgresql database configurations

---
+++ .env.ps1
@@ -0,0 +1,3 @@
+& ./venv/Scripts/Activate.ps1
+$env:APP_SETTINGS="config.DevelopmentConfig"
+$env:DATABASE_URL="postgresql://localhost/wordcount_dev"
--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config():
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-is-a-secret'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
