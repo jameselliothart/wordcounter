8c0343a1eb6155f3098a486f8aa87a8bc05dc537
James Hart <james.elliot.hart@gmail.com>
2019-10-14 14:35:22 -0500

    refactor to queue word count and send to results page

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import requests
 import operator
 import re
 import nltk
+from rq import Queue
+from rq.job import Job
+from worker import conn
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
@@ -16,56 +19,74 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+q = Queue(connection=conn)
 
 from models import Result  # noqa workaround for circular import
 
 
+def count_and_save_words(url):
+    errors = []
+
+    try:
+        r = requests.get(url)
+    except Exception:
+        errors.append(
+            "Unable to get URL. Please make sure it's valid and try again."
+        )
+        return {"error": errors}
+
+    # process text
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for script in soup(['script', 'style']):
+        script.decompose()  # remove scripts and styles
+    raw = soup.get_text()
+    nltk.data.path.append('./nltk_data/')  # set path
+    text = nltk.tokenize.word_tokenize(raw)
+
+    # remove punctuation and count raw words
+    nonPunct = re.compile('.*[A-Za-z].*')
+    raw_words = [w for w in text if nonPunct.match(w)]
+    raw_word_count = Counter(raw_words)
+
+    # stop words
+    no_stop_words = [w for w in raw_words if w.lower() not in stops]
+    no_stop_words_count = Counter(no_stop_words)
+
+    # save results
+    try:
+        result = Result(
+            url=url,
+            result_all=raw_word_count,
+            result_no_stop_words=no_stop_words_count
+        )
+        db.session.add(result)
+        db.session.commit()
+        return result.id
+    except Exception:
+        errors.append("Unable to add item to database.")
+    return {"error": errors}
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    errors = []
     results = {}
-    r = None
     if request.method == "POST":
-        try:
-            url = request.form.get('url')
-            r = requests.get(url)
-        except Exception:
-            errors.append(
-                "Unable to get URL. Please make sure it's valid and try again."
-            )
-            return render_template('index.html', errors=errors)
-    if r:
-        # process text
-        soup = BeautifulSoup(r.text, 'html.parser')
-        for script in soup(['script', 'style']):
-            script.decompose()  # remove scripts and styles
-        raw = soup.get_text()
-        nltk.data.path.append('./nltk_data/')  # set path
-        text = nltk.tokenize.word_tokenize(raw)
-        # remove punctuation and count raw words
-        nonPunct = re.compile('.*[A-Za-z].*')
-        raw_words = [w for w in text if nonPunct.match(w)]
-        raw_word_count = Counter(raw_words)
-        # stop words
-        no_stop_words = [w for w in raw_words if w.lower() not in stops]
-        no_stop_words_count = Counter(no_stop_words)
-        # save results
-        results = sorted(
-            no_stop_words_count.items(),
-            key=operator.itemgetter(1),
-            reverse=True
+        url = request.form.get('url')
+        job = q.enqueue_call(
+            func=count_and_save_words, args=(url,), result_ttl=5000
         )
-        try:
-            result = Result(
-                url=url,
-                result_all=raw_word_count,
-                result_no_stop_words=no_stop_words_count
-            )
-            db.session.add(result)
-            db.session.commit()
-        except Exception:
-            errors.append("Unable to add item to database.")
-    return render_template('index.html', errors=errors, results=results)
+        print(job.get_id())
+
+    return render_template('index.html', results=results)
+
+
+@app.route("/results/<job_key>", methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=conn)
+    if job.is_finished:
+        return str(job.result), 200
+    else:
+        return "Nay!", 202
 
 
 if __name__ == '__main__':
